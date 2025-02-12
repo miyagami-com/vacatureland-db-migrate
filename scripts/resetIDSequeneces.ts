370466dd4b0b9f1490dfa97750c1adb0cac7fc63
ybahae <ybahaerarslan@gmail.com>
2025-02-12 14:30:17 +0100

    log

--- scripts/resetIDSequeneces.ts
+++ scripts/resetIDSequeneces.ts
@@ -1,5 +1,11 @@
 import { supabase } from "../src/supabase";
 
 export const resetIDSequeneces = async () => {
-  await supabase.rpc("reset_sequences");
+  const { error } = await supabase.rpc("reset_sequences");
+
+  if (error) {
+    console.error("Error resetting sequences:", error);
+  } else {
+    console.log("Sequences reset successfully");
+  }
 };
